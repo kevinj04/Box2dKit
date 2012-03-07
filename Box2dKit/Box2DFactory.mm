d517afcd89b92f34a3bf4d7c58e03f27673d79e1
Kevin Jenkins <kevinj04@gmail.com>
2012-03-07 15:01:19 -0500

    Added in some private functions to handle universal input

--- Box2dKit/Box2DFactory.mm
+++ Box2dKit/Box2DFactory.mm
@@ -10,7 +10,52 @@
 #import "Box2DHelper.h"
 #import <UIKit/UIKit.h>
 
+#define kOffsetIPadX 0.0
+#define kOffsetIPadY 0.0
+
 static b2World *world;
+
+@interface Box2DFactory (hidden)
+- (CGPoint) scalePointForIPad:(CGPoint) p;
+- (CGPoint) scalePointFromIPad:(CGPoint) p;
++ (CGRect) scaleRectFromIPad:(CGRect) r;
++ (CGRect) scaleRectForIPad:(CGRect) r;
+@end
+
+@implementation Box2DFactory (hidden)
+- (CGPoint) scalePointForIPad:(CGPoint) p {
+    // ipad actions need to be twice as large, maybe old iphone too?
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        return CGPointMake(p.x*2.0+kOffsetIPadX, p.y*2.0+kOffsetIPadY);
+    } else {
+        return p;
+    }
+}
+- (CGPoint) scalePointFromIPad:(CGPoint) p {
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        return CGPointMake((p.x - kOffsetIPadX) *0.5, (p.y - kOffsetIPadY)*0.5);
+    } else {
+        return p;
+    }
+}
++ (CGRect) scaleRectFromIPad:(CGRect) r {
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        return CGRectMake((r.origin.x - kOffsetIPadX)*0.5, (r.origin.y-kOffsetIPadY)*0.5, r.size.width*0.5, r.size.height*0.5);
+    } else {
+        return r;
+    }
+}
++ (CGRect) scaleRectForIPad:(CGRect) r {
+    // ipad actions need to be twice as large, maybe old iphone too?
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        return CGRectMake(r.origin.x*2.0+kOffsetIPadX, r.origin.y*2.0+kOffsetIPadY, r.size.width*2.0, r.size.height*2.0);
+    } else {
+        return r;
+    }
+}
+
+
+@end
 
 @implementation Box2DFactory
 
@@ -31,7 +76,7 @@ static b2World *world;
     
     CGPoint p = CGPointMake([element position].x - offsetAP.x, 
                             [element position].y - offsetAP.y);//[element position];
-    CGRect r = [element boundingBox];
+    CGRect r = [self scaleRectFromIPad:[element boundingBox]];
     
     float ratio = [Box2DHelper pixelsToMeterRatio];
     
